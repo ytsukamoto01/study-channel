cb9e95fa1009274b88a31be6b80da777a96442f5
ytsukamoto01 <ytsukamoto0121@gmail.com>
2025-09-20 10:39:36 +0900

    Delete api/admin directory

--- api/admin/logout/route.js
+++
@@ -1,7 +0,0 @@
-// /api/admin/logout/route.js
-import { clearAdminSessionCookie } from "../../_lib/adminSession";
-
-export async function POST() {
-  clearAdminSessionCookie();
-  return new Response(JSON.stringify({ ok: true }), { status: 200 });
-}
