ace460c1fa2ae44235f28c8457ec35ec98fa0c86
ytsukamoto01 <ytsukamoto0121@gmail.com>
2025-09-20 17:51:26 +0900

    Update replies.js

--- js/replies.js
+++ js/replies.js
@@ -202,7 +202,7 @@ function renderParent(c) {
     <div class="comment-header">
       <div class="comment-header-left">
         <span class="comment-number">${c.comment_number != null ? `${c.comment_number}.` : ''}</span>
-        <span class="comment-author">${formatAuthorName(c.author_name)}</span>
+        <span class="comment-author">${formatAuthorName(c.author_name, !!c.admin_mark)}</span>
         <span class="date">${getRelativeTime(new Date(c.created_at).getTime())}</span>
       </div>
       <div class="comment-moderation-links">
@@ -270,7 +270,7 @@ function renderReplies(list) {
       <div class="reply-item" data-comment-id="${c.id}">
         <div class="comment-header">
           <div class="comment-header-left">
-            <span class="comment-author">${formatAuthorName(c.author_name)}</span>
+            <span class="comment-author">${formatAuthorName(c.author_name, !!c.admin_mark)}</span>
             <span class="date">${getRelativeTime(new Date(c.created_at).getTime())}</span>
           </div>
           <div class="comment-moderation-links">
